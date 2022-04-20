60447435d3c242fd1ce02927786c3ddf1964dd0a
LitMo <quantalcat@gmail.com>
2022-04-19 17:15:55 -0700

    Update 778. Swim in rising water.py

--- 778. Swim in rising water.py
+++ 778. Swim in rising water.py
@@ -1,3 +1,20 @@
 ## https://leetcode.com/problems/swim-in-rising-water/
 
-## Topic. 
+## Topic: BFS 
+## 
+## Dijstra similar & prior-queue
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+        pq = [(grid[0][0], 0, 0)]
+        res = 0 
+        visited = {(0,0)}
+        while pq:
+            height, x, y = heapq.heappop(pq)
+            res = max(res, height)
+            if x == y == N-1:
+                return res
+            for new_x, new_y in ((x-1, y), (x+1, y), (x, y+1), (x, y-1)):
+                if 0 <= new_x < N and 0 <= new_y < N and (new_x, new_y) not in visited:
+                    heapq.heappush(pq, (grid[new_x][new_y], new_x, new_y))
+                    visited.add((new_x,new_y))
